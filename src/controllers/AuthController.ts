fa89643be80de8f9e8afc25663fe666bc4c5b19c
Adriano Silva <adrsilva613@gmail.com>
2024-05-24 11:47:17 -0300

    [API] Cookies autenticação

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -27,14 +27,12 @@ class AuthController {
             httpOnly: true,
             secure: true,
             maxAge: 60 * 60 * 24 * 1,
-            sameSite: 'lax',
             path: '/',
           })
           .setCookie('session-company', validaUsuario.idEscola, {
             httpOnly: true,
             secure: true,
             maxAge: 60 * 60 * 24 * 1,
-            sameSite: 'lax',
             path: '/',
           })
           .send({
